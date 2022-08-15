58f6187b0f70d567fac4270a561b1147e1e61331
SanjaliNagare57 <67482074+SanjaliNagare57@users.noreply.github.com>
2022-08-15 14:52:24 -0700

    updates 3

--- src/action.js
+++ src/action.js
@@ -2,7 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github') 
 
 async function run() {
-  console.log('Hello, world!');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
 
   await octokit.issues.createComment({
     ...context.repo,
